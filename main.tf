aeb164779ea68820232d8631148e92796be1121a
Hadi Assalem <hadi.assalem@gmail.com>
2025-04-10 21:03:28 +0200

    Feat(TF): add resources(EKS cluster and node group)

--- main.tf
+++ main.tf
@@ -76,3 +76,73 @@ resource "aws_security_group" "project_0_node_sg" {
         Name = "project-0-node-sg"
     }
 }
+
+resource "aws_eks_cluster" "project_0" {
+  name = "project-0-cluster"
+  role_arn = aws_iam_role.project_0_cluster_role.arn
+  vpc_config {
+    subnet_ids = aws_subnet.project_0_subnet[*].id
+    security_group_ids = [aws_security_group.project_0_cluster_sg.id]
+  }
+}
+
+resource "aws_eks_node_group" "project_0" {
+    cluster_name = aws_eks_cluster.project_0.name
+    node_group_name = "project-0-node-group"
+    node_role_arn = aws_iam_role.project_0_node_group_role.arn
+    subnet_ids = aws_subnet.project_0_subnet[*].id
+
+    scaling_config {
+      desired_size = 3
+      max_size = 3
+      min_size = 3
+    }
+
+    instance_types = ["t2.medium"]
+    remote_access {
+      ec2_ssh_key = var.ec2_ssh_key
+      source_security_group_ids = [aws_security_group.project_0_node_sg.id]
+    }
+}
+
+resource "aws_iam_role" "project_0_cluster_role" {
+    name = "project-0-cluster-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "project_0_cluster_role_policy" {
+  role       = aws_iam_role.project_0_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+
+}
+
+resource "aws_iam_role" "project_0_node_group_role" {
+  name = "project-0-node-group-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "ec2_ssh_key" {
+    description = "SSH key-pair name for EC2 instances"
+    type        = string
+    default     = "DevSecOps"
+}
